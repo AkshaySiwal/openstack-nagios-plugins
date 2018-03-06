eac25856124fb0240c0f39c104522428130bbd1e
Akshay <akshay231990@gmail.com>
2018-03-06 16:26:23 +0530

    Update Token.py

--- openstacknagios/keystone/Token.py
+++ openstacknagios/keystone/Token.py
@@ -1,7 +1,3 @@
-#
-#    Copyright (C) 2014  Cirrax GmbH  http://www.cirrax.com
-#    Benedikt Trefzer <benedikt.trefzer@cirrax.com>
-#
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
